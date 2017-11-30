82751b216914b112841e356ed5946c6aefe067e4
Allen <learn.to.code.tw2@gmail.com>
2017-12-01 05:14:57 +0800

    list comprehension

--- read.py
+++ read.py
@@ -17,7 +17,32 @@ print('留言的平均長度為', sum_len/len(data))
 new = []
 for d in data:
 	if len(d) < 100:
-		new.append(d)
+		new.append(dR)
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+# print('一共有', len(good), '筆留言提到good')
+
+good = [1 for d in data if 'good' in d]
+print(good)
+
+bad = ['bad' in d for d in data]
+print(bad)
+
+bad = []
+for d in data:
+    bad.append('bad' in d)
+
+# count = 0
+# for review in good[:3]:
+# 	count += 1
+# 	print('這是第', count, '筆留言提到good')
+# 	print(review)
+
+
